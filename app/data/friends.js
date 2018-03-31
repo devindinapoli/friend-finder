7441d44e467069f044a6de4e0a4e2da9ca9207c0
Devin DiNapoli <devindinapoli@gmail.com>
2018-03-31 19:54:39 -0400

    had to delete repository, create new. Saved all old files and transferred to this repository

---
+++ app/data/friends.js
@@ -0,0 +1,68 @@
+var friendsList = [
+    {
+        name: "Brak",
+        photo: "http://assets2.ignimgs.com/2016/02/07/brakjpg-c7447d.jpg",
+        scores: [
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5"
+        ]
+    },
+    {
+        name: "Zorak",
+        photo: "https://i.ytimg.com/vi/H-P6EolKb90/hqdefault.jpg",
+        scores: [
+            "3",
+            "2",
+            "5",
+            "3",
+            "4",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1"   
+        ]    
+    },
+    {
+        name: "Space Ghost",
+        photo: "https://i.ytimg.com/vi/FRk_JxwKku4/hqdefault.jpg",
+        scores: [
+            "3",
+            "2",
+            "5",
+            "3",
+            "4",
+            "3",
+            "5",
+            "2",
+            "3",
+            "1"   
+        ]
+    },
+    {
+        name: "Someone's Grandma",
+        photo: "https://i.imgflip.com/9inh7.jpg?a422856",
+        scores: [
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1"   
+        ]
+    }
+];
+
+module.exports = friendsList;
