97bdcec50a842d5d404b5e746bb7dbcd96e93506
Devin DiNapoli <devindinapoli@gmail.com>
2018-04-03 12:25:42 -0400

    Added documentation

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,18 +1,20 @@
+//require path & friends.js (holds my friends array)
 var path = require("path");
-
 var friends = require("../data/friends.js");
 
+//exporting...
 module.exports = function(app){
+    //gets the information from friends..
     app.get("/api/friends", function(req,res){
         res.json(friends);
     });
-
+    //Posts to the friends api
     app.post("/api/friends", function(req,res){
 
         var userAnswers = req.body;
 
         var userScores = userAnswers.scores;
-
+        //start the algorithm to find closest "match"
         var matchName = "";
         var matchPic = "";
         var totalDiff = 1000;
@@ -30,7 +32,9 @@ module.exports = function(app){
                 matchPic = friends[i].photo;
             }
         }
+        //This will push the information the user gave into the friends array...
         friends.push(userAnswers);
+        //Then this will give the required info for my modal to show up (Won't show up if there is no information )
         res.json({status: "OK", matchName: matchName, matchPic: matchPic});
     })
 }
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,9 +1,12 @@
+//require path
 var path = require("path");
-
+// exporting...
 module.exports = function(app){
+    //using "get" to set the home page to "/"
     app.get("/", function(req,res){
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
+    //using "get" to set the survey page to "/survey"
     app.get("/survey", function(req,res){
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
